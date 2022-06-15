a7304df1aa0368cf6b3c5fd8cf224836bb4dbb2f
lyon <liang6516@outlook.com>
2022-06-15 20:29:58 +0800

    rm no used file

--- LVGL.Simulator/pikascript/pikascript-lib/pika_lvgl/pika_lvgl_lv_event.c
+++
@@ -1,8 +0,0 @@
-#include "lvgl.h"
-#include "pika_lvgl_lv_event.h"
-
-int pika_lvgl_lv_event_get_code(PikaObj *self){
-    lv_event_t *lv_event = obj_getPtr(self, "lv_event");
-    return lv_event_get_code(lv_event);
-}
-
